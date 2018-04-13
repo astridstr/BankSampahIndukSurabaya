464c88bb57bddc5b9ff43a79ef9708429175866b
Mutia Rahmi Dewi <mutia15@mhs.if.its.ac.id>
2018-04-13 16:56:52 +0700

    mutia

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Transaksi;
 
 class TransaksiController extends Controller
 {
    public function index()
     {
-        return view('menu-transaksi');
+    	$transaksi = Transaksi::orderBy('no_rekening','asc')->get();
+        return view('menu-transaksi', ['transaksi'=>$transaksi]);
+    }
+
+    public function getFormTambahTransaksi()
+    {
+        return view('form.tambah.transaksi');
+    }
+
+    public function setFormTambahTransaksi(Request $request)
+    {
+        $transaksi = new Transaksi();
+        $transaksi->no_rekening = $request->input('no_rekening');
+        $transaksi->id_sampah = $request->input('id_sampah');
+        $transaksi->kuantitas = $request->input('kuantitas');
+        $transaksi->tgl_transaksi = $request->input('tgl_transaksi');
+
+        $transaksi->save();
+
+        return redirect()->back();
     }
 }
