b52976291265492c21918fe8517a59188d4e4b3c
Mutia Rahmi Dewi <mutia15@mhs.if.its.ac.id>
2018-04-25 15:53:34 +0700

    edit transaksi kurang submit

--- resources/views/form/edit/transaksi.blade.php
+++ resources/views/form/edit/transaksi.blade.php
@@ -7,11 +7,11 @@
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
-    <h1>Form Tambah Transaksi</h1>
+    <h1>Form Edit Transaksi</h1>
     <ol class="breadcrumb">
       <li><a href="{{ url('/home') }}"><i class="fa fa-dashboard"></i> Home</a></li>
       <li><a href="{{ url('/transaksi') }}">Transaksi</a></li>
-      <li class="active">Tambah Transaksi</li>
+      <li class="active">Edit Transaksi</li>
     </ol>
   </section>
 
@@ -24,63 +24,77 @@
 
           <!-- /.box-header -->
           <!-- form start -->
-          <form role="form" action="{{url('/transaksi-tambah')}}" method="post" enctype="multipart/form-data">
+          <form role="form" action="{{ url('transaksi-update', array($transaksi->no_rekening)) }}" method="post" enctype="multipart/form-data">
             {{ csrf_field() }}
             <div class="box-body">
               <!-- no_rekening -->
               <div class="form-group">
                 <label>No. Rekening</label>
                 <input type="text" class="form-control" placeholder="No. Rekening"
-                name="no_rekening" required="">
+                name="no_rekening" required="" value="{{$transaksi->no_rekening}}">
               </div>
               <!-- id_sampah -->
               <div class="form-group">
                 <label>Input Sampah</label>
               </div>
-              <div class="form-group">
-                <div class="col-xs-5">
-                  <label>ID Sampah</label>
-                  <select class="form-control select2" style="width: 100%;">
-                    <option selected="selected">Alabama</option>
-                    <option>Alaska</option>
-                    <option>California</option>
-                    <option>Delaware</option>
-                    <option>Tennessee</option>
-                    <option>Texas</option>
-                    <option>Washington</option>
+              <table class="table table-bordered" id="dynamic_field">
+                <thead>
+                  <tr>
+                    <th>ID Sampah</th>
+                    <th>Kuantitas</th>
+                    <th>Tambah Sampah</th>              
+                  </tr>
+                </thead>
+                <tbody>
+                 <tr>
+                  <td>
+                    <input type="text" class="form-control" placeholder="ID Sampah"
+                    name="id_sampah[]" required="" value="{{$inputsampah->id_sampah}}">
                   </select>
-                </div>
-                <div class="col-xs-4">
-                  <label>Kuantitas</label>
+                </td>
+                <td>
                   <input type="text" class="form-control" placeholder="Kuantitas"
-                  name="Kuantitas" required="">
-                </div>
-                <div id="addmore"></div>
-                <div id="add" class="btn btn-success btn-xs" style="margin-top: 30px"><i class="fa fa-plus-circle"></i>Tambah Sampah</div>
-              </div>
-              <!-- /.input group -->
-            </div>
-
-            <!-- /.box-body -->
-
-            <div class="box-footer">
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-          </form>
+                  name="kuantitas[]" required="" value="{{$inputsampah->kuantitas}}">
+                </td>
+                <td>
+                  <button type="button" name="add" id="add" class="btn btn-success">Add More</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <!-- /.input group -->
         </div>
-        <!-- /.box -->
-      </div>
-      <!-- /.col -->
+
+        <!-- /.box-body -->
+
+        <div class="box-footer">
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </form>
     </div>
-    <!-- /.row -->
-  </section>
-  <!-- /.content -->
+    <!-- /.box -->
+  </div>
+  <!-- /.col -->
+</div>
+<!-- /.row -->
+</section>
+<!-- /.content -->
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script type="text/javascript">
-  $('#add').click(function(){
-    $('#addmore').append('<div class="form-group"><div class="col-xs-4"><label>ID Sampah</label><select class="form-control select2" style="width: 100%;"><option selected="selected">Alabama</option><option>Alaska</option><option>California</option><option>Delaware</option><option>Tennessee</option><option>Texas</option><option>Washington</option></select></div><div class="col-xs-4"><label>Kuantitas</label><input type="text" class="form-control" placeholder="Kuantitas"name="Kuantitas" required=""></div></div>');
-    $('.add').select2();
+  $(document).ready(function(){
+    var i=1;
+    $('#add').click(function(){
+      i++;
+      $('#dynamic_field').append('<tr id="row'+i+'"><td><input type="text" class="form-control" placeholder="ID Sampah" name="id_sampah" required=""></td><td><input type="text" class="form-control" placeholder="Kuantitas"name="kuantitas" required=""></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
+    });
+
+    $(document).on('click', '.btn_remove', function(){
+      var button_id = $(this).attr("id"); 
+      $('#row'+button_id+'').remove();
+    });
   });
 </script>
 @endsection
